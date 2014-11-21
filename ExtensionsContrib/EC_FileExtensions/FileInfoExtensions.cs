749c4ca5aa4e26de7e2645924c7160e3a6d64ae4
Britz <gbritzv@gmail.com>
2014-11-20 23:46:22 -0200

    feat: File is in use (locked)

---
+++ ExtensionsContrib/EC_FileExtensions/FileInfoExtensions.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.IO;
+using System.Runtime.InteropServices;
+
+namespace EC.FileExtensions
+{
+    /// <summary>
+    /// http://stackoverflow.com/questions/876473/is-there-a-way-to-check-if-a-file-is-in-use
+    /// </summary>
+    public static class FileInfoExtensions
+    {
+        private const Int32 ERROR_SHARING_VIOLATION = 32;
+        private const Int32 ERROR_LOCK_VIOLATION = 33;
+
+        public static Boolean IsFileLocked(this String filePath)
+        {
+            if (String.IsNullOrEmpty(filePath))
+                throw filePath == null ? new ArgumentNullException("filePath") : new ArgumentException("Parameter is empty.", "filePath");
+
+            var info = new FileInfo(filePath);
+            return info.IsLocked();
+        }
+
+        public static Boolean IsLocked(this FileInfo file)
+        {
+            if (file == null)
+                throw new ArgumentNullException("file");
+
+            FileStream stream = null;
+
+            try
+            {
+                stream = file.Open(FileMode.Open, FileAccess.ReadWrite, FileShare.None);
+            }
+            catch (DirectoryNotFoundException ex)
+            {
+                throw ex;
+            }
+            catch (FileNotFoundException ex)
+            {
+                throw ex;
+            }
+            catch (IOException ex)
+            {
+                return IsFileLocked(ex);
+            }
+            finally
+            {
+                if (stream != null)
+                    stream.Close();
+            }
+
+            //file is not locked
+            return false;
+        }
+
+        private static Boolean IsFileLocked(Exception exception)
+        {
+            var errorCode = Marshal.GetHRForException(exception) & ((1 << 16) - 1);
+            return errorCode == ERROR_SHARING_VIOLATION || errorCode == ERROR_LOCK_VIOLATION;
+        }
+    }
+}
---
+++ ExtensionsContrib/EC_FileExtensionsTest/FileInfoExtensionsTest.cs
@@ -0,0 +1,63 @@
+﻿using EC.FileExtensions;
+using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.IO;
+
+namespace ExtensionsContrib.FileTest
+{
+    [TestClass]
+    public class FileInfoExtensionsTest
+    {
+        private readonly String filePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "test.txt");
+
+        [TestInitialize]
+        public void CreateFileTest()
+        {
+            File.AppendAllText(filePath, "Test...");
+        }
+
+        [TestCleanup]
+        public void DeleteFileTest()
+        {
+            File.Delete(filePath);
+        }
+
+        [TestMethod]
+        public void File_should_be_is_in_use()
+        {
+            using (var file = File.Open(filePath, FileMode.Open)) 
+            {
+                var info = new FileInfo(filePath);
+                info.IsLocked().Should().BeTrue();
+            }
+        }
+
+        [TestMethod]
+        public void File_should_not_be_is_in_use()
+        {
+            var info = new FileInfo(filePath);
+            info.IsLocked().Should().BeFalse();
+        }
+
+        [TestMethod]
+        public void Throw_exception_when_parameter_is_null_or_invalid()
+        {
+            Action stringIsNull = () => ((String)null).IsFileLocked();
+            Action stringIsEmpty = () => String.Empty.IsFileLocked();
+            Action fileInfoIsNull = () => ((FileInfo)null).IsLocked();
+
+            stringIsNull.ShouldThrow<ArgumentNullException>()
+                .And.ParamName
+                    .Should().Be("filePath");
+
+            stringIsEmpty.ShouldThrow<ArgumentException>()
+                .And.ParamName
+                    .Should().Be("filePath");
+
+            fileInfoIsNull.ShouldThrow<ArgumentNullException>()
+                .And.ParamName
+                    .Should().Be("file");
+        }
+    }
+}
